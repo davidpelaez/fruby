1397276bc29add9f6e2898e6ebb4ed0617b155fc
David Pelaez <david@vlipco.com>
2016-03-13 18:19:42 -0500

    vendor updates

--- lib/fruby/operation/step_data.rb
+++ lib/fruby/operation/step_data.rb
@@ -1,22 +1,22 @@
 module Fruby
   class Operation
-    StepData = Struct.new(:env, :meta, :hash) do
+    StepData = Struct.new(:env, :meta, :data) do
       def respond_to?(x)
-        super(x) || hash.respond_to?(x)
+        super(x) || data.respond_to?(x)
       end
 
       def method_missing(method_sym, *args, &blk)
-        hash.send method_sym, *args, &blk
+        data.send method_sym, *args, &blk
       end
 
       def next!(*args, &blk)
-        self.class.new env, meta, *args, &blk
+        self.class.new(env, meta, *args, &blk)
       end
 
-      alias_method :to_hash, :hash
+      alias_method :to_hash, :data
 
       def inspect
-        "#<StepData @env=#{env.inspect} @hash=#{hash.inspect} @meta=#{meta.inspect}>"
+        "#<StepData @env=#{env.inspect} @data=#{data.inspect} @meta=#{meta.inspect}>"
       end
     end
   end
